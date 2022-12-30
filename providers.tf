d45d520120013fc108133e223b8d7370fc3cabf1
indika <indikaraj@hotmail.co.uk>
2022-12-30 13:42:21 +0000

    initial commit

--- providers.tf
+++ providers.tf
@@ -3,9 +3,10 @@ provider "google" {
   region  = var.region
 }
 
-terraform {
-  backend "gcs" {
-    bucket = "round-bounty-363611-tfstate"
-    prefix = "terraform/state"
-  }
-}
+#terraform {
+ # backend "gcs" {
+  #  bucket = "round-bounty-363611-tfstate"
+   # prefix = "terraform/state"
+    #credentials = ${{ secrets.GCP_ACCESS_TOKEN }}
+  #}
+#}
